71792c566bca498926c1df27539f13f83ee9be41
Fra <135766845+JungleKiosk@users.noreply.github.com>
2023-08-26 11:40:33 +0200

    setting show & hide Concept

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,15 @@ import AppHeader from './components/AppHeader.vue';
 import Jumbotron from './components/Jumbotron.vue';
 import FeaturesHistory from './components/FeaturesHistory.vue';
 import BannerCards from './components/BannerCards.vue';
+import Concept from './components/Concept.vue';
+
 
 export default {
   data() {
     return {
     };
   },
-  components: { AppHeader, Jumbotron, FeaturesHistory, BannerCards },
+  components: { AppHeader, Jumbotron, FeaturesHistory, BannerCards, Concept },
   computed: {
     linksData() {
       return links;
@@ -28,6 +30,7 @@ export default {
   <Jumbotron></Jumbotron>
   <FeaturesHistory></FeaturesHistory>
   <BannerCards :links="linksData"></BannerCards>
+  <Concept></Concept>
 </template>
 
 <style scoped></style>
---
+++ src/components/Concept.vue
@@ -0,0 +1,65 @@
+<script>
+export default {
+    data() {
+        return {
+            showContent: false, // Inizialmente nascondi il contenuto
+        };
+    },
+    methods: {
+        toggleContent() {
+            this.showContent = !this.showContent; // Inverte lo stato di visualizzazione del contenuto
+        },
+    },
+};
+</script>
+
+
+<template>
+    <div class="container my-5 txt_jumbo ">
+        <div class="row justify-content-center">
+            <div class="col-12 col-lg-6 col-md-6 bg_item rounded-4">
+                <div class="d-flex align-items-center justify-content-between p-3">
+                    <h3 class="title">Morphology & Climatic</h3>
+                    <button class="bg_btn mx-3" @click="toggleContent">{{ showContent ? 'Hide' : 'Show' }}</button>
+                </div>
+                <div v-show="showContent">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis sequi libero aliquam, aspernatur
+                        iusto at commodi reprehenderit veritatis, perferendis, obcaecati quisquam id! Sapiente nobis ipsam
+                        quaerat maxime autem obcaecati dolorum.
+                    </p>
+
+                    <div class="end_soiltext p-3">
+                        <a href="#goup">^ Go Up ^</a>
+                        <button class="bg_btn mx-3" @click="toggleContent">{{ showContent ? 'Hide' : '' }}</button>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+  
+
+<style>
+.end_soiltext a {
+    text-decoration: none;
+    color: #aaaaaa;
+}
+
+a {
+    color: #f2b91e;
+    text-decoration: underline;
+}
+
+.zoom_plots {
+    transition: transform 0.2s ease-in-out;
+    transform-origin: center center;
+}
+
+.zoom_plots:hover {
+    transform: scale(1.75);
+    border: 0;
+}
+</style>
